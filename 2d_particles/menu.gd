92f2074019a1be71840c5657a5cfda335c9923a5
Lemetist <Lemetist@gmail.com>
2024-12-08 20:50:58 +0300

    dialog

--- 2d_particles/menu.gd
+++ 2d_particles/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_up() -> void:
-	get_tree().change_scene_to_file("res://node_2d.tscn")
+	get_tree().change_scene_to_file("res://dialoge_scane.tscn")
 
 
 func _on_options_button_up() -> void:
